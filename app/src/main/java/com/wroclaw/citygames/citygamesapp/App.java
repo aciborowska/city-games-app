c095932cb9b6b2251a57249bc61a27d5cb29e2f4
Agnieszka Ciborowska <aga.ciborowska@gmail.com>
2015-09-30 22:22:37 +0200

    App upload.

---
+++ app/src/main/java/com/wroclaw/citygames/citygamesapp/App.java
@@ -0,0 +1,25 @@
+package com.wroclaw.citygames.citygamesapp;
+
+import android.app.Application;
+import android.content.Context;
+import android.util.Log;
+
+public class App extends Application {
+
+    private static final String TAG = App.class.getName();
+
+    /**
+     * Globalnie dostępny kontekst aplikacji
+     */
+    private static Context ctx = null;
+
+
+    public static Context getCtx() {
+        return ctx;
+    }
+        public void onCreate() {
+            super.onCreate();
+            this.ctx = getApplicationContext();
+            Log.d(TAG, "onCreate");
+    }
+}
